9bfc02e50908cc3b385d7a395bb5c45e8b12e0ea
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-05-01 23:47:39 +0530

    hello world

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = requres("express")
+const express = require('express')
 const app = express()
+const port = 3000
 
-app.get("/",(req,res) =>{
-    console.log("Here")
-    res.Status(500).send("Hi")
-    
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
-app.listen(3000)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
